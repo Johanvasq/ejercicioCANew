55c131913c93706e9359fbac032aa6bd6a0e0ae5
Johanvasq <johan.vasquez@ias.com.co>
2023-01-25 21:06:14 -0500

    Course Entry Point Tested

---
+++ src/test/java/co/com/ias/ejercicioCA/infrastructure/entrypoint/CourseEntryPointTest.java
@@ -0,0 +1,133 @@
+package co.com.ias.ejercicioCA.infrastructure.entrypoint;
+
+import co.com.ias.ejercicioCA.domain.model.course.dto.CourseDTO;
+import co.com.ias.ejercicioCA.domain.usecase.CourseUseCase;
+import co.com.ias.ejercicioCA.infrastructure.entrypoint.CourseEntryPoint;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcResultHandlersDsl;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(CourseEntryPoint.class)
+public class CourseEntryPointTest {
+
+    @MockBean
+    private CourseUseCase courseUseCase;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    @DisplayName("save course")
+    void saveCourse() throws Exception {
+        //Arrange
+        CourseDTO courseDTO = new CourseDTO(1L, "Fisica");
+        when(courseUseCase.saveCourse(any(CourseDTO.class)))
+                .thenReturn(courseDTO);
+        ObjectMapper mapper = new ObjectMapper();
+        //Act && Assert
+        mockMvc.perform(
+                post("/course")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(courseDTO))
+        ).andExpect(status().isCreated())
+                .andExpect(content().string(containsString(courseDTO.getName())));
+    }
+
+
+    @Test
+    @DisplayName("find course by id FOUND")
+    void findById() throws Exception {
+        saveCourse();
+        CourseDTO courseDTO = new CourseDTO(1L, "Fisica");
+
+        when(courseUseCase.findById(any(Long.class)))
+                .thenReturn(courseDTO);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        //Act && Assert
+        mockMvc.perform(
+                get("/course/1"))
+                .andExpect(status().isFound())
+                .andExpect(content().string(containsString(courseDTO.getName())));
+
+    }
+
+    @Test
+    @DisplayName("find course by id NOT_FOUND")
+    void testFindById() throws Exception {
+
+        when(courseUseCase.findById(any(Long.class)))
+                .thenReturn(any());
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        //Act && Assert
+        mockMvc.perform(
+                        get("/course/1"))
+                .andExpect(status().isNotFound());
+    }
+
+
+
+    @Test
+    @DisplayName("find all courses ok")
+    void findAll() throws Exception {
+        saveCourse();
+        CourseDTO courseDTO = new CourseDTO(1L, "Fisica");
+
+        when(courseUseCase.findAll())
+              .thenReturn(List.of(courseDTO));
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        //Act && Assert
+        mockMvc.perform(
+                get("/course"))
+                .andExpect(status().isFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].name", is("Fisica")));
+
+    }
+
+    @Test
+    @DisplayName("not found courses")
+    void testFindAll() throws Exception {
+
+        when(courseUseCase.findAll())
+                .thenReturn(List.of());
+
+        mockMvc.perform(
+                        get("/course"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Course not found"));
+    }
+
+
+}
